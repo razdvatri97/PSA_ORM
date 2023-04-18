97b0da861f105ff24ff1cec4cd4da8f558deff59
Josias Melo Vidal <Josias.Vidal@edu.pucrs.br>
2023-04-17 21:37:02 -0300

    commit inicial

---
+++ src/main/java/com/pucrs/psaorm/PsaOrmApplication.java
@@ -0,0 +1,13 @@
+package com.pucrs.psaorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucrs/psaorm/PsaOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucrs.psaorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsaOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
